b7617248c1b5b27302b15c893d3326e7bae08fcd
Nuriddin <nuriddin_2468@mail.ru>
2019-09-20 15:59:19 +0500

    Adding registration and local db

--- staticPages/urls.py
+++ staticPages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from staticPages import views
 
+
 urlpatterns = [
-    path('dashboard/', views.indexs)
+    path('dashboard/', views.indexs, name="dashboard"),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView, name='logout'),
+    path('registration/', views.RegisterView.as_view(), name='register')
 ]
